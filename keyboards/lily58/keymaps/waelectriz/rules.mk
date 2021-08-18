17ef9acc27bee8b90aa8d65faf06f9f5a6cef73c
Ng Chuan Seng <waelectriz@gmail.com>
2021-08-18 18:54:36 +0800

    Update keymap
    Included bongo cats based on WPM (From noroadsleft) in right OLED and custom layer naming in left OLED

--- keyboards/lily58/keymaps/waelectriz/rules.mk
+++ keyboards/lily58/keymaps/waelectriz/rules.mk
@@ -16,6 +16,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. 
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE= yes     # OLED display
+WPM_ENABLE = yes            # 
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
@@ -25,6 +26,6 @@ SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
+        ./lib/timelogger.c \
         # ./lib/mode_icon_reader.c \
         # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
